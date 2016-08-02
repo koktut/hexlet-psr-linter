085eaa332285682d0ce733baf9d9725d545d0a8b
koktut <koktut@mail.ru>
2016-08-02 22:22:08 +0600

    Interface for rules

--- src/Linter/Rules.php
+++ src/Linter/Rules.php
@@ -1,31 +1,15 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Гуля
+ * Date: 02.08.2016
+ * Time: 22:10
+ */
 
 namespace HexletPsrLinter\Linter;
 
-/**
- * Class Rules
- * @package PsrLinter
- */
+
 class Rules
 {
-    /**
-     * @param $name
-     * @return bool
-     */
-    public function validateFunctionName($name)
-    {
-        if ($name == '__construct') {
-            return true;
-        }
-        return \PHP_CodeSniffer::isCamelCaps($name, false, true, true);
-    }
 
-    /**
-     * @param $name
-     * @return bool
-     */
-    public function validateVariableName($name)
-    {
-        return \PHP_CodeSniffer::isCamelCaps($name);
-    }
 }
