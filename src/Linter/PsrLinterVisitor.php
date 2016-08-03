b4a8b1c196cadda98db9329bd4777a955f44ce94
Maxim Sobin <maxim.sobin@gmail.com>
2016-08-03 15:45:44 +0600

    Add visitorMode

--- src/Linter/PsrLinterVisitor.php
+++ src/Linter/PsrLinterVisitor.php
@@ -3,6 +3,7 @@
 namespace HexletPsrLinter\Linter;
 
 use PhpParser\Node;
+use PhpParser\NodeDumper;
 use PhpParser\NodeVisitorAbstract;
 use PhpParser\ParserFactory;
 use PhpParser\Node\Stmt;
@@ -16,9 +17,13 @@ use HexletPsrLinter\Logger\LogRecord;
  */
 class PsrLinterVisitor extends NodeVisitorAbstract
 {
+    const VISITOR_MODE_LINTER = 1;
+    const VISITOR_MODE_SIDE_EFFECTS = 2;
+
     private $parser;
     private $rules;
     private $logger;
+    private $visitorMode;
 
     /**
      * PsrLinter constructor.
@@ -37,9 +42,14 @@ class PsrLinterVisitor extends NodeVisitorAbstract
     public function lint($code)
     {
         $this->logger = new Logger();
+
+        $this->visitorMode = self::VISITOR_MODE_LINTER;
         $stmts = $this->parser->parse($code);
         $traverser = new NodeTraverser;
         $traverser->addVisitor($this);
+        $traverser->traverse($stmts);
+
+        $this->visitorMode = self::VISITOR_MODE_SIDE_EFFECTS;
         $traverser->traverse($stmts);
 
         return $this->logger;
@@ -51,25 +61,56 @@ class PsrLinterVisitor extends NodeVisitorAbstract
      * @param Node $node
      * @return void
      */
-    public function leaveNode(Node $node)
+    public function enterNode(Node $node)
     {
-        foreach ($this->rules as $rule) {
-            $vaildator = new $rule;
+        switch ($this->visitorMode) {
+            case self::VISITOR_MODE_LINTER:
+                foreach ($this->rules as $rule) {
+                    $vaildator = new $rule;
 
-            $result = $vaildator->validate($node);
+                    $result = $vaildator->validate($node);
 
-            if ($result !== true) {
-                list($level, $message) = $result;
-                $this->logger->addRecord(
-                    new LogRecord(
-                        $node->getAttribute('startLine'),
-                        0,
-                        $level,
-                        $message,
-                        $node->name
-                    )
-                );
-            }
+                    if ($result !== true) {
+                        list($level, $message) = $result;
+                        $this->logger->addRecord(
+                            new LogRecord(
+                                $node->getAttribute('startLine'),
+                                0,
+                                $level,
+                                $message,
+                                $node->name
+                            )
+                        );
+                    }
+                }
+                break;
+            case self::VISITOR_MODE_SIDE_EFFECTS:
+                if ($this->visitorMode == self::VISITOR_MODE_SIDE_EFFECTS && !($node instanceof Stmt\Namespace_)) {
+                    return NodeTraverser::DONT_TRAVERSE_CHILDREN;
+                }
+                break;
         }
+    }
+
+    /**
+     * @param $node
+     */
+    private function isDeclaration($node)
+    {
+    }
+
+    /**
+     * @param $node
+     * @return mixed
+     */
+    private function isSideEffect($node)
+    {
+        $sideEffectNodeTypes = [
+            Node\Expr\Include_::class,
+            Node\Expr\FuncCall::class,
+            Node\Stmt\Echo_::class
+        ];
+
+        return in_array($node, $sideEffectNodeTypes);
     }
 }
